b131a03051ebf026ee5227e206c96b36f66efa53
davidflast <davidflasterstein@wustl.edu>
2016-04-03 22:55:15 -0500

    readInData
    Function works now. I just need to add column headings since I don’t
    know what the columns represent

--- readInData.R
+++ readInData.R
@@ -9,6 +9,7 @@
 #' @note Sometimes the server will fail to send the data to your computer, 
 #'      so this function will try again until it successfully connects to the server.
 #' @examples
+#' 
 #' x <- 204:208
 #' myData <- readInData(x) 
 #' y <- c(204, 206, 208, 207)
@@ -29,6 +30,7 @@ readInDataHelper <- function(batchNumber) {
   for(k in 1:ncol(hold)){
     if(is.factor(hold[,k])) hold[,k] <- as.character(hold[,k])
   }
+  return(hold)
 }
 #' @export
 readInData <- function(batchNumbers) {
@@ -39,12 +41,12 @@ readInData <- function(batchNumbers) {
   require(plyr)
   # I use adply because it is easier to do non-sequential 
   # batch numbers with apply than with a for loop and it allows me to
-  # control inputs into the function.
+  # control inputs and outputs from function.
   output <- adply(.data=batchNumbers, 
                   .fun=readInDataHelper, .margins=1)
   #https://sentimentit.com/api/batches/1/download.json 
-  batch_names <- paste("batch ", batchNumbers)
-  colnames(output) <- batch_names
+  # TODO: Column names for data frame
+  colnames(output) <- c("number","batch number ordered", "batch number")
   return(output)
 }
 
