f82894e730c272852d943c979e0acccf88b7def3
Joseph Ludmir <jludmir@wustl.edu>
2016-04-04 22:35:28 -0500

    created package doc

---
+++ R/sentimentIt-package.R
@@ -0,0 +1,10 @@
+#' sentimentIt
+#'
+#' The sentimentIt package performs functions for the sentimentIt project.  
+#' @name sentimentIt
+#' @docType package
+#' @author  David Carlson
+#' @examples 
+#' 
+#'
+NULL
