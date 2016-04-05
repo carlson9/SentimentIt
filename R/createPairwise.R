dac95d8e6fe7644ce000aca7c1abe65f392a4bc4
Joseph Ludmir <jludmir@wustl.edu>
2016-04-04 21:38:22 -0500

    Start of package creation

---
+++ R/createPairwise.R
@@ -0,0 +1,45 @@
+#' Creates documents with two texts to be compared.
+#'
+#' @param ids The id numbers of the texts you want to use.
+#' @param number_per How many documents per batch to be compared.
+#'
+#' @return Creates a document with the texts to be compared.
+#'
+#' @author David Carlson
+#'
+#' @note This function requires the usage of the plyr package.
+#' @rdname createPairwise
+createPairwise <- function(ids, number_per){
+  documents <- unique(as.numeric(ids))
+  require(plyr)
+  # This code sets up the random pairwise comparisons
+  pairwise<-cbind(rep(documents, (number_per+1)%/%2), matrix(replicate((number_per+1)%/%2, sample(documents)), ncol=1))
+  duplicates<-pairwise[which(pairwise[,1]==pairwise[,2]),]
+  
+  # some ugly code to keep documents from being compared with themselves
+  if(!is.null(nrow(duplicates))){
+    while(nrow(duplicates)>1){
+      duplicates[,1]<-duplicates[sample(1:nrow(duplicates)),1]
+      pairwise[which(pairwise[,1]==pairwise[,2]),]<-duplicates
+      duplicates<-matrix(pairwise[which(pairwise[,1]==pairwise[,2]),], ncol=2)
+    }	
+    if(nrow(duplicates)==1){
+      oneIndex<-which(pairwise[,1]==pairwise[,2])
+      oneValue<-pairwise[oneIndex,2]
+      twoIndex<-sample(c(1:nrow(pairwise))[pairwise[,2]!=unique(c(duplicates))], 1)
+      twoValue<-pairwise[twoIndex,2]
+      pairwise[oneIndex,2]<-twoValue
+      pairwise[twoIndex,2]<-oneValue
+    }
+  }else{
+    if(is.vector(duplicates)){
+      oneIndex<-which(pairwise[,1]==pairwise[,2])
+      oneValue<-pairwise[oneIndex,2]
+      twoIndex<-sample(c(1:nrow(pairwise))[pairwise[,2]!=unique(c(duplicates))], 1)
+      twoValue<-pairwise[twoIndex,2]
+      pairwise[oneIndex,2]<-twoValue
+      pairwise[twoIndex,2]<-oneValue
+    }
+  }
+  return(pairwise)
+}
