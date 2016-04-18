b8360543dbc00fd00ea8c0376ab900cf5435dba1
tmuraoka320 <tmuraoka@wustl.edu>
2016-04-18 16:31:13 -0500

    extractCoef demo

--- demo/extractCoef.R
+++ demo/extractCoef.R
@@ -1,2 +1,3 @@
 load("~/SentimentIt/data/fit_stan_hierDataExample.rda")
+fit_hier <- fitStanHier(data=ExampleData_hier1, hierarchy_data=ExampleData_hier2, hierarchy_var="countries")
 extractCoef(fit_hier, ExampleData_hier2, "ids", "countries")
