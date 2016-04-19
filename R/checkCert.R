0bd4130edc6230895baaa01344322ee73cc6d48c
jludmir88 <jludmir@wustl.edu>
2016-04-19 13:53:49 -0500

    Update checkCert.R

--- R/checkCert.R
+++ R/checkCert.R
@@ -10,15 +10,13 @@
 #' @rdname checkCert
 #' @export
 createCert <- function(cert, workers){
-  require(jsonlite)
-  require(httr)
   out <- vector()
   args <- list(certification = cert, workers = workers)
   args <- toJSON(args, auto_unbox=TRUE)
   URL <- paste0("http://sentimentit.herokuapp.com/api/certifications/", as.character(cert),
-         "/turk_workers/", as.character(workers), ".json")
+          "/turk_workers/", as.character(workers), ".json")
   mypost <- POST(URL, body = args, content_type_json(),
-                 encode='json')
+          encode='json')
   out <- c(out, unlist(fromJSON(rawToChar(as.raw(mypost$content)))))
   return(out)
 }
