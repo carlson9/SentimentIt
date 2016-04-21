140a4b43cf3b85209bf1f171ab99feec0eae7218
jludmir88 <jludmir@wustl.edu>
2016-04-21 13:46:28 -0500

    Update createHITSBatch.R

--- R/createHITSBatch.R
+++ R/createHITSBatch.R
@@ -3,10 +3,10 @@
 #' Creates batches, and waits a certain time based on the progress of batch.
 #'
 #' @param batches Vector of Batch numbers to check.
-#' @param min_time Earliest that hits can be created.
-#' @param max_time Latest time that hits can be created.
-#' @param threshold Point at which a batch is considered done.
-#' @param rate The rate by which the progress of a batch will be checked.
+#' @param min_time Earliest that hits can be created (default = 9:00).
+#' @param max_time Latest time that hits can be created (default = 22:00).
+#' @param threshold Point at which a batch is considered done (default = 5).
+#' @param rate The rate by which the progress of a batch will be checked (default = 1/3 an hour).
 #' 
 #' 
 #' @return out ID for batch of comparisons
@@ -16,22 +16,16 @@
 #'
 #' @rdname createHITSBatch
 #' @export
-createHITSBatch <- function(batches, min_time, 
-                   max_time, time_per, threshold, checkWorkersAt=NULL){
+createHITSBatch <- function(batches, min_time=9, 
+                   max_time=22, rate=1/3, threshold=5, checkWorkersAt=NULL){
   out <- vector()
   for(i in batches){
     checkTime(min_time, max_time)
     x <- createHITS(batch_id=i)
     out[i] <- x
     done <- FALSE
-    Sys.sleep(time_per*3600)
-    for(i in batches){
-      checkTime(min_time, max_time)
-      x <- createHITS(batch_id=i)
-      out[i] <- x
-      done <- FALSE
       while(!done){
-        Sys.sleep(rate*60)
+        Sys.sleep(rate*3600)
         status <- batchStatus(batches[i])
         done <- ((status$submitted_count - status$completed_count) <= threshold)
       }
@@ -41,6 +35,7 @@ createHITSBatch <- function(batches, min_time,
   }
   return(out)
 }
+#' @export
 checkTime <- function(min_time, max_time){
   current <- as.numeric(format(Sys.time(), "%H"))
   if(current>=max_time) Sys.sleep((24-current+min_time)*3600)
