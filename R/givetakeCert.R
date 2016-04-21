37f3433546911c8e608f6475ff2725c55f54a380
jludmir88 <jludmir@wustl.edu>
2016-04-21 13:51:53 -0500

    Update givetakeCert.R

--- R/givetakeCert.R
+++ R/givetakeCert.R
@@ -13,18 +13,17 @@
 #' @export
 givetakeCert <- function(certone, certtwo, workers){
   if(!is.character(certone) | nchar(certtwo)<1){
-    stop("You must input a non-blank certification and one made of characters."
+    stop("You must input a non-blank certification and one made of characters.")
   }
   if(!is.character(certtwo) | nchar(certtwo)<1){
-    stop("You must input a non-blank certification and one made of characters."
+    stop("You must input a non-blank certification and one made of characters.")
   }
   if(certone == certtwo){
-    stop("The certifications you are giving and taking away cannot be made the same."
+    stop("The certifications you are giving and taking away cannot be made the same.")
   }
   if(!is.character(workers) | nchar(workers)<1){
-    stop("You must input a non-blank worker id and one made of characters."
+    stop("You must input a non-blank worker id and one made of characters.")
   }
   createCert(certone, workers)
   revokeCert(certtwo, workers)
 }
-#' @export
