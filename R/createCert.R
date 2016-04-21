65b45d4240981db7319f034b3c2c55ceb8e8c64e
jludmir88 <jludmir@wustl.edu>
2016-04-21 13:53:47 -0500

    Update createCert.R

--- R/createCert.R
+++ R/createCert.R
@@ -11,10 +11,10 @@
 #' @export
 createCert <- function(cert, workers){
   if(!is.character(cert) | nchar(cert)<1){
-    stop("You must input a non-blank certification and one made of characters."
+    stop("You must input a non-blank certification and one made of characters.")
   }
   if(!is.character(workers) | nchar(workers)<1){
-    stop("You must input a non-blank certification and one made of characters."
+    stop("You must input a non-blank certification and one made of characters.")
   }
   out <- vector()
   args <- list(certification = cert, workers = workers)
@@ -25,4 +25,3 @@ createCert <- function(cert, workers){
   out <- c(out, unlist(fromJSON(rawToChar(as.raw(mypost$content)))))
   return(out)
 }
-#' @export
