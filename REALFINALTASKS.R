ae91e6e916a8808a7ed7e56c6f55a5cba3a183b0
jludmir88 <jludmir@wustl.edu>
2016-04-26 13:46:17 -0500

    Create REALFINALTASKS.R

---
+++ REALFINALTASKS.R
@@ -0,0 +1,9 @@
+Final Project Goals:  TO BE DONE BY THE END 
+
+- Finish documentations for all functions and highest level wrapper (sentimentIt). 
+- Hide medium level wrappers to users. (DAVID)
+- Finish sentimentIt wrapper (PRIORITY ONE) (JOE)
+- Change all "HITS" to be "tasks". (DAVID)
+- Fix all parameter names to be more specific (path name) (JOE)
+- Make sure function parameters and defaults match throughout.  (TOGETHER NOW)
+- Fix check workers function. (TAISHI)
