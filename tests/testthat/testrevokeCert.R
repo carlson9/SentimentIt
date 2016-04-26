1d4997534c428d158e0bfa038e954cf76c6c25f9
jludmir88 <jludmir@wustl.edu>
2016-04-26 11:02:46 -0500

    Update testrevokeCert.R

--- tests/testthat/testrevokeCert.R
+++ tests/testthat/testrevokeCert.R
@@ -1,4 +1,9 @@
 context("revokeCert")
-# Inputs a radnom worker ID as the constant.
+
 test_that("revokeCert throws an error for non-character inputs",{
-  expect_error(revokeCert(3
+  expect_error(revokeCert(3, "abc"), "certification must be a set of characters, not just a number")
+  expect_error(revokeCert(3, ""), "you must input something for the worker id")
+  expect_error(revokeCert("", "abd"), "you must input something for the certification")
+  expect_error(revokeCert("abc", 3), "the worker id must be a set of characters, not just a number")
+})
+  
