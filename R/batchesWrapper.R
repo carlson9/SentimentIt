846565d099340b9e361e3274de1cf1aa4aecbda5
tmuraoka320 <tmuraoka@wustl.edu>
2016-04-26 09:42:07 -0500

    add n.core

--- R/batchesWrapper.R
+++ R/batchesWrapper.R
@@ -4,7 +4,7 @@
 #'
 #' @param pathFrom What file path the data will be drawn form, or actual data
 #' @param hit_setting_id ID of HIT setting to use
-#' @param question the question the worker will see once the worker selects the HIT 
+#' @param question the question the worker will see once the worker selects the HIT
 #' @param timed If True, HITS will be created by time, if not by batch
 #' @param pathTo Where to send the text to be reviewed to.
 #' @param what The text to be sent and used in the data frame.
@@ -30,17 +30,17 @@
 #' @param chains The number of chains (default is 3)
 #' @param iter The number of iteration (default is 2500)
 #' @param seed Set seed (default is 1234)
-#' @param n.cores Number of cores to be used in stan wrapper(default is 3)
-#' 
+#' @param n.cores Number of cores to be used in stan fit (default is 3)
+#'
 #' @return batches A vector of batch numbers that have been created
-#' 
+#'
 #' @author Jacob M. Montgomery
 #'
 #' @seealso \code{\link{batchStatus}}, \code{\link{createHITSTimed}}, \code{\link{checkCert}},\code{\link{checkWorkers}},\code{\link{createBatches}},
 #' \code{\link{createCert}},\code{\link{createHITS}}, \code{\link{createHITSBatch}},\code{\link{createPairwise}}, \code{\link{timedWrapper}},
 #' \code{\link{extractCoef}},\code{\link{fitStan}},\code{\link{fitStanHier}},\code{\link{givetakeCert}},\code{\link{makeCompsSep}},
-#' \code{\link{readInData}}, \code{\link{readText}},\code{\link{repostExpired}},\code{\link{revokeCert}},\code{\link{stanWrapper}} 
-#' @example 
+#' \code{\link{readInData}}, \code{\link{readText}},\code{\link{repostExpired}},\code{\link{revokeCert}},\code{\link{stanWrapper}}
+#' @example
 #' \dontrun{
 #' batchesWrapper(timed=TRUE, hit_setting_id=2,question=,pathfrom="/dropbox/documents/questions")
 #'}
@@ -49,9 +49,9 @@
 #'
 #' @export
 batchesWrapper <- function(pathfrom, hit_setting_id, question,
-                           timed=TRUE, pathto=NULL, what=’character’,
-                           sep=’\n’, quiet=TRUE,
-                           index=NULL, which_source=’apiR’,
+                           timed=TRUE, pathto=NULL, what="character",
+                           sep="\n", quiet=TRUE,
+                           index=NULL, which_source="apiR",
                            number_per=20, per_batch=1000,
                            path=NULL, name=NULL,
                            time_per=1, mintime=9, maxtime=22,
@@ -62,9 +62,9 @@ batchesWrapper <- function(pathfrom, hit_setting_id, question,
                            returnFit=FALSE, plot=FALSE, file=NULL,
                            chains=3, iter=2500,
                            seed=1234, n.cores=3, ...){
-  
-  # read text into API 
-  if(pathto=NULL){
+
+  # read text into API
+  if(is.null(pathto)){
   textDoc <- readText(pathfrom=pathfrom, pathto=pathto, what=what, sep=sep, quiet=quiet,
                   index=index, which_source=which_source, ...)
   } else {
@@ -79,15 +79,15 @@ batchesWrapper <- function(pathfrom, hit_setting_id, question,
   makeCompsSep(ids=textDoc$ids, number_per=number_per, batches=batches, question=question,
                path=path, name=name)
   Sys.sleep(rest_time)
-  
+
   # Create HITS for each of the created batches
   if(timed){
    createHITSTimed(batches=batches, time_per=time_per, mintime=mintime, maxtime=maxtime,
                     checkWorkersAt=batches[checkWorkersAt], certone=certone, certtwo=certtwo,
-                   hierarchy_data=hierarchy_data,  hierarchy_var=hierarchy_var, returnFit=returnFit, 
-                   plot=plot, file=file, chains=chains, iter=iter, seed=seed)
+                   hierarchy_data=hierarchy_data,  hierarchy_var=hierarchy_var, returnFit=returnFit,
+                   plot=plot, file=file, chains=chains, iter=iter, seed=seed, n.core=n.core)
   } else{
-    createHITSBatch(batches=batches, min_time=min_time, max_time=max_time, 
+    createHITSBatch(batches=batches, min_time=min_time, max_time=max_time,
                            rate=rate, threshold=threshold, checkWorkersAt=batches[checkWorkersAt])
   }
   return(batches)
