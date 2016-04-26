bcb495abf332f0b140026bc66fca102fa56bd0fa
jludmir88 <jludmir@wustl.edu>
2016-04-26 14:08:20 -0500

    Update sentimentIt.R

--- R/sentimentIt.R
+++ R/sentimentIt.R
@@ -6,7 +6,7 @@
 #' @param pathFrom Where the text will be drawn from
 #' @param task_setting_id ID of task setting to use
 #' @param question Where to separate text by line
-#' @param waitToRepost Time before reposting expired tasks
+#' @param timed Default is TRUE, this uses a by time checking system as opposed to by batch progress.
 #' @param pathTo Where to send the text to be reviewed to
 #' @param what The text to be sent and used in the data frame
 #' @param sep Where to separate text by line
@@ -14,40 +14,27 @@
 #' @param index The index number
 #' @param which_source What type of file is the text being drawn from
 #' @param number_per How many documents per batch to be compared
-#' @param per_batch If true, this does not print the amount of items read prior
-
-
-#' @param timed HITS are replaced by time, not batch status
-#' @param hit_setting_id ID of HIT setting to use
-#' @param num_batches number of batches to create using the HIT setting
-#' @param pathFrom Where the text will be drawn from
-#' @param pathTo Where to send the text to be reviewed to
-=======
-#' @param timed Tasks are replaced by time, not batch status
-#' @param hit_setting_id ID of Task setting to use
-#' @param num_batches number of batches to create using the Task setting
-#' @param readDocumentsFrom Where the text will be drawn from
-#' @param writeDocumentsTo Where to send the text to be reviewed to
->>>>>>> Stashed changes
-#' @param what The text to be sent and used in the data frame
-#' @param sep Where to separate text by line
-#' @param quiet If true, this does not print the amount of items read prior
-#' @param index The index number
-#' @param which_source What type of file is the text being drawn from
-#' @param number_per How many documents per batch to be compared
-#' @param batches The number of batches to be made
-#' @param question Where to separate text by line
 #' @param per_batch If true, this does not print the amount of items read prior
 #' @param path File path
 #' @param name File name
-#' @param idsAsComps IDs as comparison
 #' @param time_per Defalt is 1
 #' @param mintime Defalt is 8
 #' @param maxtime Defalt is 22
 #' @param certone Defalt is NULL
 #' @param certtwo Defalt isNULL
 #' @param checkWorkersAt Defalt is NULL
-#' @param rest_time Defalt is 60
+#' @param rest_time Default is 60
+#' @param rate Default is 1/3
+#' @param threshold Default is 5
+#' @param hierarchy_data A file that contains the variable that is used as a hierarchy (default is NULL)
+#' @param hierarchy_var A name of the variable in \code{hierarchy_data} that is used as a hierarchy (default is NULL)
+#' @param returnFit Return a fit object if TRUE (default is FALSE)
+#' @param plot If TRUE, create a histogram with a rug plot (default is FALSE)
+#' @param file Save the histogram to path and file name specified (default is NULL)
+#' @param chains The number of chains (default is 3)
+#' @param iter The number of iteration (default is 2500)
+#' @param seed Set seed (default is 1234)
+#' @param n.cores Number of cores to be used in stan fit (default is 3)
 #'
 #' @return
 #'
