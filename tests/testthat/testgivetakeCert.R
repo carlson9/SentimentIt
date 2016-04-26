1b45993d8874e58fc14cd69963d2d4bd96293f7d
jludmir88 <jludmir@wustl.edu>
2016-04-26 11:12:27 -0500

    Update testgivetakeCert.R

--- tests/testthat/testgivetakeCert.R
+++ tests/testthat/testgivetakeCert.R
@@ -1,10 +1,12 @@
-test_that("revokeCert throws an error for non-character inputs, blank inputs, and the same certifications",{
+context("givetakeCert")
 
-  expect_error(revokeCert(3, "abd", "abc"), "certification must be a set of characters, not just a number")
-  expect_error(revokeCert("abd", 3, "abc"), "certification must be a set of characters, not just a number")
-  expect_error(revokeCert(3, "", "abd"), "you must input something for the certification")
-  expect_error(revokeCert("", 3, "abd"), "you must input something for the certification")
-  expect_error(revokeCert("abd", "abd", "abc"), "the two certifications must be different")
-  expect_error(revokeCert("abc", "abd", 3), "the worker id must be a set of characters, not just a number")
-  expect_error(revokeCert("abc", "abd", ""), "you must input something for the worker id")
+test_that("givetakeCert throws an error for non-character inputs, blank inputs, and the same certifications",{
+
+  expect_error(givetakeCert(3, "abd", "abc"), "certification must be a set of characters, not just a number")
+  expect_error(givetakeCert("abd", 3, "abc"), "certification must be a set of characters, not just a number")
+  expect_error(givetakeCert(3, "", "abd"), "you must input something for the certification")
+  expect_error(givetakeCert("", 3, "abd"), "you must input something for the certification")
+  expect_error(givetakeCert("abd", "abd", "abc"), "the two certifications must be different")
+  expect_error(givetakeCert("abc", "abd", 3), "the worker id must be a set of characters, not just a number")
+  expect_error(givetakeCert("abc", "abd", ""), "you must input something for the worker id")
 })
