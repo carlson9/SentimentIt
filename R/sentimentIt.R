4ce9f57e530a2e0495406604ec55f37ecebd6408
davidflast <davidflasterstein@wustl.edu>
2016-04-26 14:13:43 -0500

    sentimentIt

--- R/sentimentIt.R
+++ R/sentimentIt.R
@@ -2,35 +2,16 @@
 #'
 #' A wrapper function of batchesWrapper and repostExpired
 #'
-<<<<<<< Updated upstream
-#' @param pathFrom Where the text will be drawn from
-#' @param task_setting_id ID of task setting to use
-#' @param question Where to separate text by line
-#' @param timed Default is TRUE, this uses a by time checking system as opposed to by batch progress.
-#' @param pathTo Where to send the text to be reviewed to
-#' @param what The text to be sent and used in the data frame
-#' @param sep Where to separate text by line
-#' @param quiet If true, this does not print the amount of items read prior
-#' @param index The index number
-#' @param which_source What type of file is the text being drawn from
-#' @param number_per How many documents per batch to be compared
-#' @param per_batch If true, this does not print the amount of items read prior
-<<<<<<< Updated upstream
-=======
-
-
 #' @param timed HITS are replaced by time, not batch status
 #' @param task_setting_id ID of HIT setting to use
 #' @param num_batches number of batches to create using the HIT setting
 #' @param pathFrom Where the text will be drawn from
 #' @param pathTo Where to send the text to be reviewed to
-=======
 #' @param timed Tasks are replaced by time, not batch status
 #' @param task_setting_id ID of Task setting to use
 #' @param num_batches number of batches to create using the Task setting
 #' @param readDocumentsFrom Where the text will be drawn from
 #' @param writeDocumentsTo Where to send the text to be reviewed to
->>>>>>> Stashed changes
 #' @param what The text to be sent and used in the data frame
 #' @param sep Where to separate text by line
 #' @param quiet If true, this does not print the amount of items read prior
@@ -40,7 +21,6 @@
 #' @param batches The number of batches to be made
 #' @param question Where to separate text by line
 #' @param per_batch If true, this does not print the amount of items read prior
->>>>>>> Stashed changes
 #' @param path File path
 #' @param name File name
 #' @param time_per Defalt is 1
