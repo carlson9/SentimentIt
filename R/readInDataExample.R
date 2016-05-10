ee9570494148054128bdcc8c266092d60fd5e325
davidflast <davidflasterstein@wustl.edu>
2016-05-09 20:41:19 -0500

    see also

--- R/readInDataExample.R
+++ R/readInDataExample.R
@@ -12,6 +12,12 @@
 #'   \item{worker_id}{id of the worker who did the comparison}
 #'   \item{completed_at}{time comaparison was completed}
 #' }
+#' @seealso \code{\link{createTasksTimed}}, \code{\link{batchesWrapper}}, \code{\link{checkCert}},
+#' \code{\link{checkWorkers}},\code{\link{createBatches}},\code{\link{createCert}},\code{\link{createTasks}}, 
+#' \code{\link{createPairwise}}, \code{\link{extractCoef}},\code{\link{fitStan}},\code{\link{fitStanHier}},
+#' \code{\link{givetakeCert}},\code{\link{makeCompsSep}},\code{\link{readInData}}, \code{\link{readText}},
+#' \code{\link{repostExpired}},\code{\link{revokeCert}}
+#' }
 #' @author Jacob M. Montgomery
 #' @source
 #'
