bed6ad2d9fceac68cb590bb6b94dde5eceeb8ae0
carlson9 <carlson.david@wustl.edu>
2017-08-29 16:34:05 -0500

    Adressing some CRAN check issues. Still need to change encoding of data.

---
+++ sentimentIt.Rcheck/00_pkg_src/sentimentIt/R/makeCompsSep.R
@@ -0,0 +1,68 @@
+#' Create comparisons and post to SentimentIt
+#'
+#' Creates random comparisons of document IDs using \code{\link{createPairwise}}. These comparisons are posted on the SentimentIt server and receive unique IDs from the system.
+#'
+#' @param email The researcher's email used for SentimentIt registration
+#' @param password The researcher's password used for SentimentIt registration
+#' @param ids Numerical vector of IDs for the documents
+#' @param number_per Number of comparisons desired
+#' @param batch_id Batch IDs to be used for the tasks
+#' @param question A character of the question the worker will see once the worker selects the task
+#' @param per_batch Number of comparisons per batch desired. Defaulted to 1000.
+#' @param pairwise_path File path to store matrix of document IDs used for comparisons. Default is pairwise.Rdata
+#' @param ids_as_comps an indicator as to whether or not the IDs provided refer to comparison IDs rather than document IDs. Default is FALSE.
+#'
+#' @return out A table with the text and correspondings ID's that have been sent.
+#'
+#' @author David Carlson
+#' @examples 
+#' 
+#' \dontrun{
+#' docInfo <- read.table(email, password, "ReviewsWithIds",header=TRUE)
+#' makeCompsSep(email = 'researcher@school.edu', password = 'uniquePassword', ids = docInfo[,'ids'], number_per = 10, batch_id = batch_ids, question = 'Below is text taken from two movie reviews. Please choose the text that you think comes from the most positive review', pairwise_path = 'Comparisons/first10.Rdata')
+#' }
+#' @seealso \code{\link{sentimentIt}} \code{\link{authenticate}} \code{\link{batchStatus}} \code{\link{checkCert}} \code{\link{checkWorkers}} \code{\link{createBatches}} \code{\link{createCert}} \code{\link{createPairwise}} \code{\link{createTasks}} \code{\link{fitStan}} \code{\link{fitStanHier}} \code{\link{readInData}} \code{\link{readText}} \code{\link{repostExpired}} \code{\link{reviews}} \code{\link{revokeCert}} \code{\link{signout}}
+#' @rdname makeCompsSep
+#' @export
+makeCompsSep <- function(email, password, ids, number_per, batch_id, question, per_batch=1000, 
+                         pairwise_path='pairwise.Rdata', ids_as_comps=FALSE){
+  if(!ids_as_comps){
+    pairwise <- createPairwise(ids, number_per, pairwise_path)
+  }else{
+    pairwise <- ids
+  }
+  num_comps <- dim(pairwise)[1]
+  if(length(batch_id)*per_batch<num_comps){
+    print('Not enough batch_id supplied')
+    return(NULL)
+  }
+  out <- vector()
+  auth_token <- authenticate(email, password)
+  if(per_batch > num_comps){
+    args <- list(email=email, auth_token=auth_token, question=question, ids=pairwise, batch_id=batch_id[1])
+    args <- toJSON(args, auto_unbox=TRUE)
+    mypost <- POST('https://www.sentimentit.com/api/comparisons/create.json',
+                   body = args, content_type_json(),
+                   encode='json')
+    out <- c(out, unlist(fromJSON(rawToChar(as.raw(mypost$content)))))
+  }else{
+    for(i in 1:(num_comps%/%per_batch)){
+      args <- list(email=email, auth_token=auth_token, question=question, ids=pairwise[((i-1)*per_batch+1):(i*per_batch),], batch_id=batch_id[i])
+      args <- toJSON(args, auto_unbox=TRUE)
+      mypost <- POST('https://www.sentimentit.com/api/comparisons/create.json',
+                   body = args, content_type_json(),
+                   encode='json')
+      out <- c(out, unlist(fromJSON(rawToChar(as.raw(mypost$content)))))
+    }
+    if(num_comps%%per_batch!=0){
+      args <- list(email=email, auth_token=auth_token, question=question, ids=pairwise[((i)*per_batch+1):(dim(pairwise)[1]),], batch_id=batch_id[i+1])
+      args <- toJSON(args, auto_unbox=TRUE)
+      mypost <- POST('https://www.sentimentit.com/api/comparisons/create.json',
+                   body = args, content_type_json(),
+                   encode='json')
+      out <- c(out, unlist(fromJSON(rawToChar(as.raw(mypost$content)))))
+    }
+  }
+  return(out)
+}
+
---
+++ sentimentIt.Rcheck/sentimentIt-Ex.R
@@ -0,0 +1,344 @@
+pkgname <- "sentimentIt"
+source(file.path(R.home("share"), "R", "examples-header.R"))
+options(warn = 1)
+base::assign(".ExTimings", "sentimentIt-Ex.timings", pos = 'CheckExEnv')
+base::cat("name\tuser\tsystem\telapsed\n", file=base::get(".ExTimings", pos = 'CheckExEnv'))
+base::assign(".format_ptime",
+function(x) {
+  if(!is.na(x[4L])) x[1L] <- x[1L] + x[4L]
+  if(!is.na(x[5L])) x[2L] <- x[2L] + x[5L]
+  options(OutDec = '.')
+  format(x[1L:3L], digits = 7L)
+},
+pos = 'CheckExEnv')
+
+### * </HEADER>
+library('sentimentIt')
+
+base::assign(".oldSearch", base::search(), pos = 'CheckExEnv')
+cleanEx()
+nameEx("batchStatus")
+### * batchStatus
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: batchStatus
+### Title: Check the status of batch(es)
+### Aliases: batchStatus
+
+### ** Examples
+
+
+## Not run: 
+##D  
+##D batchStatus(email = 'researcher@school.edu', password = 'uniquePassword', batch_id = batch_ids[1])
+## End(Not run)
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("batchStatus", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("checkCert")
+### * checkCert
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: checkCert
+### Title: Checks if a worker has a certification
+### Aliases: checkCert
+
+### ** Examples
+
+
+## Not run: 
+##D  
+##D x <- "ab1"
+##D y <- c("a204")
+##D check <- checkCert(email, password, x, y)
+## End(Not run)
+
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("checkCert", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("checkWorkers")
+### * checkWorkers
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: checkWorkers
+### Title: Detect outlying workers
+### Aliases: checkWorkers
+
+### ** Examples
+
+
+## Not run: 
+##D ban_workers <- checkWorkers(stan_fit = fit, data = output)
+## End(Not run)
+
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("checkWorkers", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("createBatches")
+### * createBatches
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: createBatches
+### Title: Create new batches
+### Aliases: createBatches
+
+### ** Examples
+
+## Not run: 
+##D batch_ids <- createBatches(email = 'researcher@school.edu', password = 'uniquePassword', task_setting_id = 8, num_batches = 3)
+## End(Not run)
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("createBatches", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("createCert")
+### * createCert
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: createCert
+### Title: Grants certifications to workers
+### Aliases: createCert
+
+### ** Examples
+
+
+## Not run: 
+##D  
+##D createCert(email = 'researcher@school.edu', password = 'uniquePassword', cert= 'bannedmovie_reviews', workers = ban_workers)
+## End(Not run)
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("createCert", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("createTasks")
+### * createTasks
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: createTasks
+### Title: Create new tasks for comparisons
+### Aliases: createTasks
+
+### ** Examples
+
+
+## Not run: 
+##D createTasks(email = 'researcher@school.edu', password = 'uniquePassword', batch_id = batch_ids[1])
+## End(Not run)
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("createTasks", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("fitStan")
+### * fitStan
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: fitStan
+### Title: Fit a Stan model with results from SentimentIt
+### Aliases: fitStan
+
+### ** Examples
+
+
+## Not run: 
+##D fit <- fitStan(data = batch_ids)
+##D fit <- fitStan(data = output)
+## End(Not run)
+
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("fitStan", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("makeCompsSep")
+### * makeCompsSep
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: makeCompsSep
+### Title: Create comparisons and post to SentimentIt
+### Aliases: makeCompsSep
+
+### ** Examples
+
+
+## Not run: 
+##D docInfo <- read.table(email, password, "ReviewsWithIds",header=TRUE)
+##D makeCompsSep(email = 'researcher@school.edu', password = 'uniquePassword', ids = docInfo[,'ids'], number_per = 10, batch_id = batch_ids, question = 'Below is text taken from two movie reviews. Please choose the text that you think comes from the most positive review', pairwise_path = 'Comparisons/first10.Rdata')
+## End(Not run)
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("makeCompsSep", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("readInData")
+### * readInData
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: readInData
+### Title: Read in data provided by the workers
+### Aliases: readInData
+
+### ** Examples
+
+
+## Not run: 
+##D output <- readInData(email = 'researcher@school.edu', password = 'uniquePassword', batch_id = batch_ids[1])
+## End(Not run)
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("readInData", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("readText")
+### * readText
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: readText
+### Title: Find/create documents and retrieve IDs
+### Aliases: readText
+
+### ** Examples
+
+
+## Not run: 
+##D data(reviews)
+##D 
+##D docInfo <- readText(email = 'researcher@school.edu', password = 'uniquePassword', read_documents_from = reviews, write_documents_to = "ReviewsWithIds.csv", index = 'Review')
+## End(Not run)
+
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("readText", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("repostExpired")
+### * repostExpired
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: repostExpired
+### Title: Repost expired tasks from a batch
+### Aliases: repostExpired
+
+### ** Examples
+
+## Not run: 
+##D repostExpired(email = 'researcher@school.edu', password = 'uniquePassword', batch_id = batch_ids)
+## End(Not run)
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("repostExpired", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("revokeCert")
+### * revokeCert
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: revokeCert
+### Title: Revoke a certification for workers
+### Aliases: revokeCert
+
+### ** Examples
+
+
+## Not run: 
+##D  
+##D revokeCert(email = 'researcher@school.edu', password = 'uniquePassword', cert = 'snippets', workers = ban_workers)
+## End(Not run)
+
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("revokeCert", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+cleanEx()
+nameEx("sentimentIt")
+### * sentimentIt
+
+flush(stderr()); flush(stdout())
+
+base::assign(".ptime", proc.time(), pos = "CheckExEnv")
+### Name: sentimentIt
+### Title: sentimentIt
+### Aliases: sentimentIt sentimentIt-package sentimentIt
+
+### ** Examples
+
+
+## Not run: 
+##D data(reviews)
+##D movies <- sentimentIt(email = 'researcher@school.edu',
+##D    password = 'uniquePassword',
+##D    read_documents_from = reviews,
+##D    write_documents_to = 'ReviewsWithIds.csv',
+##D    index = 'Review', task_setting_id = 8,
+##D    number_per = 10,
+##D    question = 'Below is text taken from
+##D        two movie reviews. Please
+##D        choose the text that you
+##D        think comes from the most
+##D        positive review',
+##D    pairwise_path = 'Comparisons.Rdata',
+##D    certone = 'snippets', certtwo = 'bannedmovie_reviews',
+##D    timed = FALSE, check_workers_at = c(1,2),
+##D    rest_time = 60, rate = 1/3, threshold = 5,
+##D    return_stan = TRUE, return_data = TRUE)
+## End(Not run)
+
+
+
+base::assign(".dptime", (proc.time() - get(".ptime", pos = "CheckExEnv")), pos = "CheckExEnv")
+base::cat("sentimentIt", base::get(".format_ptime", pos = 'CheckExEnv')(get(".dptime", pos = "CheckExEnv")), "\n", file=base::get(".ExTimings", pos = 'CheckExEnv'), append=TRUE, sep="\t")
+### * <FOOTER>
+###
+options(digits = 7L)
+base::cat("Time elapsed: ", proc.time() - base::get("ptime", pos = 'CheckExEnv'),"\n")
+grDevices::dev.off()
+###
+### Local variables: ***
+### mode: outline-minor ***
+### outline-regexp: "\\(> \\)?### [*]+" ***
+### End: ***
+quit('no')
